7b7e078aba5a9ca5dc05e5891a43cd236854d556
0xevm1 <0xevm1@gmail.com>
2024-02-06 06:55:30 -0800

    style points, fixing banner

--- app/page.tsx
+++ app/page.tsx
@@ -9,9 +9,17 @@ export default function Home() {
         <div className="bg-filler main-container">
 
             {/* Top Banner */}
-            <div className="bg-orange-700 text-sm text-white text-center py-[0.15rem]">
-               Now serving Los Angeles/OC | <span className="text-gray-300">Read more</span>
+            <div className="text-xs text-[#F2F2F2] text-center py-2 bg-[#D17B49]">
+                <span className="inline-flex items-center justify-center">
+                    Same Day CPI is now serving Los Angeles/OC
+                    <span className="text-gray-300 text-xs mx-2">|</span>
+                    Read more
+                    <svg viewBox="0 0 10 10" aria-hidden="true" className="ml-1 h-2.5 w-2.5 opacity-60 transition duration-300 ease-[cubic-bezier(0.4,0.36,0,1)]">
+                    <path fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="m7.25 5-3.5-2.25v4.5L7.25 5Z"></path>
+                    </svg>
+                </span>
             </div>
+
 
             {/* Navigation Bar */}
 
