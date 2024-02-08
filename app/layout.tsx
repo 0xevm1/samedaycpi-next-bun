d033691c99c770b789006709f172428d0281b588
0xevm1 <0xevm1@gmail.com>
2024-02-08 11:15:27 -0800

    site manifest

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ import Head from "next/head";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
+  manifest: "https://www.samedaycpi.com/site.webmanifest"
   title: "Same Day CPI",
   description: "Expedited CPI Evaluations for CBRS & FCC Compliance",
   openGraph: {
